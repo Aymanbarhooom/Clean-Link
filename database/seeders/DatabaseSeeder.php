8c77998425bb02a28705af2bdd464c9227e0d077
Aymanbarhooom <aymanbarhooom@gmail.com>
2026-07-10 07:45:44 +0300

    avaliable_slots/automatic signing tasks v2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,9 +27,9 @@ class DatabaseSeeder extends Seeder
             ServiceImageSeeder::class,
             ReviewSeeder::class,
             WorkerSeeder::class,
-            WorkGroupeSeeder::class,
-            OrderSeeder::class,
-            TaskSeeder::class,
+            //WorkGroupeSeeder::class,
+            //OrderSeeder::class,
+            //TaskSeeder::class,
         ]);
     }
 }
