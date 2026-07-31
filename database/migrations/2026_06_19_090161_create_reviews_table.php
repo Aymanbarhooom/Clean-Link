b3793227f11433f2c65e523c2ec0d3fd7d8edd19
Aymanbarhooom <aymanbarhooom@gmail.com>
2026-07-31 19:10:56 +0300

    review comment not null

--- database/migrations/2026_06_19_090161_create_reviews_table.php
+++ database/migrations/2026_06_19_090161_create_reviews_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignId('client_id')->constrained('users')->onDelete('cascade');
-            $table->text('comment')->nullable();
+            $table->text('comment')->nullable()->default("");
             $table->tinyInteger('rating')->unsigned();
             
             // Polymorphic Columns for Service & Company models
