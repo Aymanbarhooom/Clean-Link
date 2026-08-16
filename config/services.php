56d1e389d99b9e07bf73491274cadc1c5eab7d44
Aymanbarhooom <aymanbarhooom@gmail.com>
2026-08-16 14:29:27 +0300

    stripe v3

--- config/services.php
+++ config/services.php
@@ -42,6 +42,7 @@ return [
     'stripe' => [
         'secret' => env('STRIPE_SECRET_KEY'),
         'key' => env('STRIPE_PUBLISHABLE_KEY'),
+        'webhook_secret' => env('STRIPE_WEBHOOK_SECRET')
     ],
 
 ];
