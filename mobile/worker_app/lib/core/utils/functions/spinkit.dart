dca04a97a03fb02c885786ac0db58600732ea420
ayman abrhoom <aymanbarhooom@gmail.com>
2026-08-28 17:32:50 +0300

    Delete mobile directory

--- mobile/worker_app/lib/core/utils/functions/spinkit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-Widget spinKitApp(Color color) {
-  return SpinKitFadingCircle(
-    color: color,
-    size: 35,
-  );
-}
